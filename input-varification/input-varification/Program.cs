189863f56005c706a6c20c5ab7c0e9695101d8ad
Titus Moore <titusdmoore@gmail.com>
2019-09-28 10:58:20 -0400

    init commit

---
+++ input-varification/input-varification/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace input_varification {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Enter your phone number: ");
+            var correctInput = true;
+            var phone = "";
+            do {
+                phone = Console.ReadLine();
+                var match = @"1?\s?\(?(\d{3})\)?\s?-?\s?(\d{3})\s?-?\s?(\d{4})";
+                Regex r = new Regex(match);
+                correctInput = r.IsMatch(phone);
+                if (!correctInput) {
+                    Console.WriteLine("Please enter a valid number!");
+                }
+            } while (!correctInput);
+            var cs = phone.ToCharArray();
+            var _out = "";
+            for (int i = 0; i < cs.Length; i++) {
+                if (i == 0) {
+                    if (cs[i] == '1') {
+                        _out += "1 ";
+                    }
+                    _out += "(";
+                }
+                var i2 = i;
+                _out += cs[i];
+                while (i2 % 3 == 0) {
+                    _out += " - ";
+                }
+            }
+            Console.WriteLine(_out);
+        }
+    }
+}
